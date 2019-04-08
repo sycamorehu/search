2613daf3c4f4141897eb18f51cfe2da6a252fbad
synamore <zyhu@gatech.edu>
2019-04-08 11:56:57 -0400

    with encode function and io

--- code/crw_md5.py
+++ code/crw_md5.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3.7
 
 import hashlib
+from pylab import *
 
-hotellist = [b'479628', b'16197084', b'22755589', b'393916', b'387009',
- b'2608420', b'427622', b'473770', b'436187', b'1073814', b'5926952', 
- b'21349561', b'4399431', b'441607', b'441585', b'374487', b'6874402', 
- b'15018773', b'15018492', b'427259', b'430295', b'14074638', b'456909', 
- b'1496646', b'12782071']
-for id in hotellist:
-    result = hashlib.md5(id)
-    print(result.hexdigest())
+dt = genfromtxt("../data/tb_1030id.csv",dtype = str, delimiter = ',')
+print(dt.shape)
+print(dt[1:5,])
+
+hashid = []
+for i in range(len(dt[:,0])):
+    hashid.append(hashlib.md5(dt[i,0].encode()).hexdigest())
+print(hashid[:10])
+
+f = open('../data/tb_1030hashed.csv', 'a')
+for i in range(dt.shape[0]):
+    f.write('%s\t' % dt[i,0])
+    f.write('%s\t' % dt[i,1])
+    f.write('%s\t' % dt[i,2])
+    f.write('%s\n' % hashid[i])
+f.close()
